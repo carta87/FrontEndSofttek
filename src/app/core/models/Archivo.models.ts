d573f0b39d7c80fda3ad88f30df3ee0673b4eae4
carta87 <cart_albert@hotmail.com>
2022-03-15 08:50:06 -0500

    create

---
+++ src/app/core/models/Archivo.models.ts
@@ -0,0 +1,7 @@
+export interface Person{
+  id:number;
+  name : string;
+  lastName  : string;
+  dni : number;
+  employee : boolean
+}
---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: true,
+
+  Url_apiBD : 'https://softtekbackend.herokuapp.com/api/persona/'
+};
